7f4255d98166c5d6b9372ce62c2ff2ecae7fee88
Nikolay Dimitrov <141030298+nikolay-dimitrov256@users.noreply.github.com>
2023-10-01 11:30:54 +0300

    Create 02_keyword_arguments_length.py

---
+++ 04_adv_functions_advanced_exercise/02_keyword_arguments_length.py
@@ -0,0 +1,12 @@
+def kwargs_length(**kwargs):
+
+    return len(kwargs)
+
+
+dictionary = {'name': 'Peter', 'age': 25}
+print(kwargs_length(**dictionary))
+
+print()
+
+dictionary = {}
+print(kwargs_length(**dictionary))
