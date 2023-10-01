0b6fdfc0fae0c6d58824b91295e9ac124a633dac
Nikolay Dimitrov <141030298+nikolay-dimitrov256@users.noreply.github.com>
2023-10-01 12:32:14 +0300

    Create 06_function_executor.py

---
+++ 04_adv_functions_advanced_exercise/06_function_executor.py
@@ -0,0 +1,37 @@
+def func_executor(*args):
+    result = ''
+
+    for item in args:
+        func = item[0]
+        arguments = item[1]
+
+        result += f'{func.__name__} - {func(*arguments)}\n'
+
+    return result
+
+
+def sum_numbers(num1, num2):
+    return num1 + num2
+
+def multiply_numbers(num1, num2):
+    return num1 * num2
+
+print(func_executor(
+    (sum_numbers, (1, 2)),
+    (multiply_numbers, (2, 4))
+))
+
+print()
+
+def make_upper(*strings):
+    result = tuple(s.upper() for s in strings)
+    return result
+
+def make_lower(*strings):
+    result = tuple(s.lower() for s in strings)
+    return result
+
+print(func_executor(
+    (make_upper, ("Python", "softUni")),
+    (make_lower, ("PyThOn",)),
+))
