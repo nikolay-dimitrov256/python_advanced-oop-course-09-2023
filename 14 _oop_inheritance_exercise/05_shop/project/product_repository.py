2e7093e345737b6fec01b49e4adabb694fdb89a4
Nikolay Dimitrov <141030298+nikolay-dimitrov256@users.noreply.github.com>
2023-11-04 21:39:24 +0200

    Solution to task

---
+++ 14 _oop_inheritance_exercise/05_shop/project/product_repository.py
@@ -0,0 +1,25 @@
+from project.product import Product
+
+
+class ProductRepository:
+    def __init__(self):
+        self.products = []
+
+    def add(self, product: Product):
+        self.products.append(product)
+
+    def find(self, product_name: str):
+        for p in self.products:
+            if p.name == product_name:
+                return p
+
+    def remove(self, product_name: str):
+        for p in self.products:
+            if p.name == product_name:
+                self.products.remove(p)
+                break
+
+    def __repr__(self):
+        result = [f'{p.name}: {p.quantity}' for p in self.products]
+
+        return '\n'.join(result)
