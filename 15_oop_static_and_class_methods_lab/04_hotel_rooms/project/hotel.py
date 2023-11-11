0fcfe2419b89753655632c359a0f19260fd7c065
Nikolay Dimitrov <141030298+nikolay-dimitrov256@users.noreply.github.com>
2023-11-11 20:49:36 +0200

    Solution to task

---
+++ 15_oop_static_and_class_methods_lab/04_hotel_rooms/project/hotel.py
@@ -0,0 +1,40 @@
+from project.room import Room
+from typing import List
+
+
+class Hotel:
+    def __init__(self, name: str):
+        self.name = name
+        self.rooms: List[Room] = []
+
+    @property
+    def guests(self):
+        return sum(r.guests for r in self.rooms)
+
+    @classmethod
+    def from_stars(cls, stars_count: int):
+        name = f'{stars_count} stars Hotel'
+        return cls(name)
+
+    def add_room(self, room: Room):
+        self.rooms.append(room)
+
+    def take_room(self, room_number, people):
+        room = next((r for r in self.rooms if r.number == room_number), None)
+
+        if room:
+            room.take_room(people)
+
+    def free_room(self, room_number):
+        room = next((r for r in self.rooms if r.number == room_number), None)
+
+        if room:
+            room.free_room()
+
+    def status(self):
+        result = f'Hotel {self.name} has {self.guests} total guests\nFree rooms: '
+        result += ', '.join([str(r.number) for r in self.rooms if not r.is_taken])
+        result += '\nTaken rooms: '
+        result += ', '.join([str(r.number) for r in self.rooms if r.is_taken])
+
+        return result
