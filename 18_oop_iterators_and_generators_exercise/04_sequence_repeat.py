50f5332b698a11bfdc59090f4d5f62e30eaf2147
Nikolay Dimitrov <141030298+nikolay-dimitrov256@users.noreply.github.com>
2023-11-22 19:32:48 +0200

    Create 04_sequence_repeat.py

---
+++ 18_oop_iterators_and_generators_exercise/04_sequence_repeat.py
@@ -0,0 +1,25 @@
+class sequence_repeat:
+    def __init__(self, sequence, number):
+        self.sequence = sequence
+        self.number = number
+        self.counter = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.counter == self.number:
+            raise StopIteration()
+
+        index = self.counter % len(self.sequence)
+        self.counter += 1
+        return self.sequence[index]
+
+
+result = sequence_repeat('abc', 5)
+for item in result:
+    print(item, end ='')
+print()
+result = sequence_repeat('I Love Python', 3)
+for item in result:
+    print(item, end ='')
