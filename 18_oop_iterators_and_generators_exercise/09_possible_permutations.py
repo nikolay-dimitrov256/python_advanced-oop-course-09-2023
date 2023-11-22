a65a20fd11816309dae6d49d9848bccd624be28f
Nikolay Dimitrov <141030298+nikolay-dimitrov256@users.noreply.github.com>
2023-11-22 20:37:30 +0200

    Create 09_possible_permutations.py

---
+++ 18_oop_iterators_and_generators_exercise/09_possible_permutations.py
@@ -0,0 +1,13 @@
+def possible_permutations(ls: list):
+    if len(ls) <= 1:
+        yield ls
+    else:
+        for i in range(len(ls)):
+            for perm in possible_permutations(ls[:i] + ls[i+1:]):
+                yield [ls[i]] + perm
+
+
+#[print(n) for n in possible_permutations([1, 2, 3])]
+
+for n in possible_permutations([1, 2, 3]):
+    print(n)
